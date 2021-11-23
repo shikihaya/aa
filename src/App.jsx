e983e3a3eba3c811ba4ce56a8f3904ad8a846685
shikihaya <94736168+shikihaya@users.noreply.github.com>
2021-11-23 14:56:47 +0900

    full

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import "./styles.css";
+import React from "react";
+import { useState, useCallback, useMemo } from "react";
+import { ChildArea } from "./ChildArea";
+
+export const App = () => {
+  console.log("App");
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const onChangeText = (event) => {
+    setText(event.target.value);
+  };
+
+  const onClickOpen = () => {
+    setOpen(!open);
+  };
+
+  const onClickClose = useCallback(() => setOpen(false), []);
+
+  const temp = useMemo(() => 1 + 3, []);
+  console.log(temp);
+
+  return (
+    <div className="App">
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} onClickClose={onClickClose} />
+    </div>
+  );
+};
---
+++ src/ChildArea.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { memo } from "react";
+
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
+};
+
+export const ChildArea = memo((props) => {
+  console.log("ChildArea");
+  const { open, onClickClose } = props;
+
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
+        </div>
+      ) : null}
+    </>
+  );
+});
